44188c511aeb6096a9f7060ef33a71945bb5c89f
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-07-06 17:54:14 +0530

    completed NAVBAR with scroll animation effect

---
+++ src/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./HomeScreen.css";
+import Nav from "./Nav";
+
+const HomeScreen = () => {
+  return (
+    <div className="homeScreen">
+      {/* NAV */}
+      <Nav />
+
+      {/* Banner */}
+      {/* ROW */}
+    </div>
+  );
+};
+
+export default HomeScreen;
