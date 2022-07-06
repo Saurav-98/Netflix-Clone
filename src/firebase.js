8e372bc19146f973f11e5be40702e576cbae33e8
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-07-06 17:02:42 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBUr5nE_8QxPfY2Vd6hkS606KUn61UM7-Y",
+  authDomain: "netflix-clone-app-263d1.firebaseapp.com",
+  projectId: "netflix-clone-app-263d1",
+  storageBucket: "netflix-clone-app-263d1.appspot.com",
+  messagingSenderId: "739938008383",
+  appId: "1:739938008383:web:24938cca1dc12a7a05278a",
+};
